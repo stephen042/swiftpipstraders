a18ef1e963c8ae13d56ee05f22797536b540ae69
stephen042 <makuochukwu042@gmail.com>
2024-04-29 13:50:17 +0100

    update

--- _servers/db_conn.php
+++ _servers/db_conn.php
@@ -1,34 +1,12 @@
 <?php
 
-// function connect_to_database()
-// {
-//     $conn_details = [
-//         "host" => "localhost",
-//         "user" => "u119630305_bitpips",
-//         "password" => "u119630305_Bitpips",
-//         "database" => "u119630305_bitpips",
-//         "port" => "3308"
-//     ];
-
-//     $connection = new mysqli(
-//         $conn_details["host"],
-//         $conn_details["user"],
-//         $conn_details["password"],
-//         $conn_details["database"],
-//         $conn_details["port"]
-//     );
-
-//     return $connection;
-// }
-
-
 function connect_to_database()
 {
     $conn_details = [
         "host" => "localhost",
-        "user" => "root",
-        "password" => "",
-        "database" => "s-code",
+        "user" => "u119630305_bitpips",
+        "password" => "u119630305_Bitpips",
+        "database" => "u119630305_bitpips",
         "port" => "3308"
     ];
 
@@ -42,3 +20,25 @@ function connect_to_database()
 
     return $connection;
 }
+
+
+// function connect_to_database()
+// {
+//     $conn_details = [
+//         "host" => "localhost",
+//         "user" => "root",
+//         "password" => "",
+//         "database" => "s-code",
+//         "port" => "3308"
+//     ];
+
+//     $connection = new mysqli(
+//         $conn_details["host"],
+//         $conn_details["user"],
+//         $conn_details["password"],
+//         $conn_details["database"],
+//         $conn_details["port"]
+//     );
+
+//     return $connection;
+// }
